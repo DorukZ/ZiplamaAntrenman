be9b4f6920dd3dc5a34b4fa739d4355cf27fd97a
DorukZ <59766201+DorukZ@users.noreply.github.com>
2025-07-19 11:48:40 +0300

    Add files via upload

---
+++ common.js
@@ -0,0 +1,23 @@
+
+const BIN_ID = "687a13b96213bd325afbbe5e";
+const API_KEY = "$2a$10$sNHjrdpRXLmd4/CDDtyxZOsljKtoyVaXwUTlb4/GSqVxF.XyAwfj2";
+const BASE_URL = "https://api.jsonbin.io/v3/b/" + BIN_ID;
+
+async function fetchData() {
+  const res = await fetch(BASE_URL + "/latest", {
+    headers: { "X-Master-Key": API_KEY }
+  });
+  const json = await res.json();
+  return json.record;
+}
+
+async function saveData(data) {
+  await fetch(BASE_URL, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Master-Key": API_KEY
+    },
+    body: JSON.stringify(data)
+  });
+}
