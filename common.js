07ee015192ee62c41b461e17473b0a054508c68a
DorukZ <59766201+DorukZ@users.noreply.github.com>
2025-07-25 21:46:16 +0300

    Update common.js

--- common.js
+++ common.js
@@ -1,7 +1,7 @@
 
 const BIN_ID = "687a13b96213bd325afbbe5e";
 const API_KEY = "$2a$10$PcJkIv48pesO0aLFSpA13OZaEjbtZ0YvCdYGfzehuQsHfySO7OSKi";
-const BASE_URL = "https://api.jsonbin.io/v3/b/" + BIN_ID;
+const BASE_URL = "https://api.jsonbin.io/v3/b/687a13b96213bd325afbbe5e";
 
 async function fetchData() {
   const res = await fetch(BASE_URL + "/latest", {
